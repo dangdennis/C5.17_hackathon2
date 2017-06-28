aeae2be5461312fac595535c2eebca900c592dc3
Howard Kim <howkim2@yahoo.com>
2017-06-28 15:13:31 -0700

    adjust photos

--- images.js
+++ images.js
@@ -1,32 +1,52 @@
 function generateImages(){
 	//generates images
-	var correctId = Math.floor(Math.random(0,3)+1);
-	var correctImg = ['cat','dog','lake'];
+	var correctId = Math.floor((Math.random()*2)+1);
+    var correctAnswers = ['cat','dog','bird'];
+    var correctPicture = correctAnswers[correctId]
 	var row = $('<div>').attr({
 		'class':'col-md-12 row'
 	});
+
 	for(var imageId = 0; imageId < 4; imageId++){
-		// 
-		// if(imageId = correctId){
-		// 	var img $('<img>').attr({
-		// 		'src' : //ajax call
-		// 		'class': 'img_sizing col-md-2',
-		// 		'data-id': imageId
-		// 	})
-		// }
+		
+		if(imageId == correctId){
+			var img = $('<img>').attr({
+				'src' : 'https://source.unsplash.com/800x700/?' + correctPicture,
+				'class': 'img_sizing col-md-3',
+				'data-id': imageId,
+				'onclick': 'checkPhoto()'
+			});
+		row.append(img);
+		}
+		else{
 		//add IMGURL from AJAX call
-		//var imgUrl;
+		//'https://source.unsplash.com/400x300/?' + correctPicture;
 
 		var img = $('<img>').attr({
 			'src': 'https://source.unsplash.com/random/800x70'+imageId,
 			'class': 'img_sizing col-md-3',
+			'onclick': 'checkPhoto()',
 			'data-id': imageId
 		});
 		row.append(img);
+		}
 
 	}
 
 	// if(imageId == 5 && )
 	$('#container').append(row);
+	return correctId;
 
+
+}
+
+function checkPhoto(correctId){
+	var correctId = generateImages();
+	console.log(correctId);
+}
+function blurMore(num){
+    var windowCondition = 4;
+    var score = 0;
+    var blurScale = num;
+    $("body").css("filter","blur("+blurScale+"px)");
 }
